e291c9c473df471feacdcd3148057f602bae0ff8
Paulo Vieira <paullocostta86@gmail.com>
2022-09-03 00:08:44 -0300

    feat: Added allowed hosts

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -32,8 +32,13 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
+# DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'https://jobgenious.paulorcvieira.vercel.app',
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
