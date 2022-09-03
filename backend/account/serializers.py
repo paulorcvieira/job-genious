0223f95afa6ff58d28e8865d3303f2850e3c343f
Paulo Vieira <paullocostta86@gmail.com>
2022-09-02 21:50:12 -0300

    feat: Created serializer

---
+++ backend/account/serializers.py
@@ -0,0 +1,39 @@
+from dataclasses import field
+
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+
+class SignUpSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'first_name',
+            'last_name',
+            'email',
+            'username',
+            'password',
+        ]
+
+        extra_kwargs = {
+            'first_name': {'required': True, 'allow_blank': False},
+            'last_name': {'required': True, 'allow_blank': False},
+            'email': {'required': True, 'allow_blank': False},
+            'password': {
+                'required': True,
+                'allow_blank': False,
+                'min_length': 6,
+            },
+        }
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'first_name',
+            'last_name',
+            'email',
+            'username',
+            'resume',
+        )
