7d9e0174cd2c69bdf8eaa51239a242ad212f9843
Paulo Vieira <paullocostta86@gmail.com>
2022-09-07 02:09:37 -0300

    feat: Created utils

---
+++ frontend/src/utils/jobOptions.ts
@@ -0,0 +1,27 @@
+export const jobTypeOptions = [
+  'Permanent',
+  'Temporary',
+  'Internship'
+]
+
+export const educationOptions = [
+  'Bachelors',
+  'Masters',
+  'PhD'
+]
+
+export const industryOptions = [
+  'Business',
+  'Information Technology',
+  'Banking',
+  'Education/Training',
+  'Telecommunication',
+  'Others'
+]
+
+export const experienceOptions = [
+  'No Experience',
+  '1 Year',
+  '2 Years',
+  '3 Years Above'
+]
